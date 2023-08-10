578f158ffe17e5bf198d549d70ff699137677232
Neel Dedhia <neeldedhia10@gmail.com>
2023-08-10 17:26:29 +0530

    update: added urls from drive api to database

--- cof/retina/urls.py
+++ cof/retina/urls.py
@@ -11,50 +11,13 @@ from django.urls import re_path, path
 
 
 app_name = 'retina'
-
-
 urlpatterns = [
     # /retina/
-    re_path(r'^$', views.index, name='index'),
+    # re_path(r'^$', views.index, name='index'),
     path('', views.index, name='index'),
-
-    # /retina/cf/
-    re_path(r'^cf/', views.cf, name='cf'),
-
-    # /retina/ma/
-    re_path(r'^ma/$', views.ma, name='ma'),
-
-    # /retina/rh/
-    re_path(r'^rh/$', views.rh, name='rh'),
-
-    # /retina/he/
-    re_path(r'^he/$', views.he, name='he'),
-
-    # /retina/cws/
-    re_path(r'^cws/$', views.cws, name='cws'),
-
-    # /retina/nve/
-    re_path(r'^nve/$', views.nve, name='nve'),
-
-    # /retina/nvd/
-    re_path(r'^nvd/$', views.nvd, name='nvd'),
-
-    # /retina/sh/
-    re_path(r'^sh/$', views.sh, name='sh'),
-
-    # /retina/vh/
-    re_path(r'^vh/$', views.vh, name='vh'),
 
     # /retina/last/
     re_path(r'^last/$', views.last, name='last'),
-
-    # /retina/<patient_id>/
-    re_path(r'^(?P<patient_id>[0-9]+)/$', views.detail, name='detail'),
-
-    # /retina/<patient_id>/process/
-    re_path(r'^(?P<patient_id>[0-9]+)/process/$',
-            views.process, name='process'),
-
 ]
 
 urlpatterns += staticfiles_urlpatterns()
